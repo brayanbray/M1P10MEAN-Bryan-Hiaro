de369e63b1def9e585b4d6dfe09cedb262055a56
Hinaox <Hiaronathanael178@gmail.com>
2023-01-30 03:16:27 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+<<<<<<< HEAD
   @Output() pageChange = new EventEmitter<string>();
   // title = 'm1p10mean-bryan-hiaro';
   currentPage = 'login';
@@ -17,6 +18,19 @@ export class AppComponent {
 
   constructor(public authService: AuthService, public apiService: ApiService) {}
   ngOnInit() {
+=======
+  constructor(public authService: AuthService,public apiService: ApiService) {}
+
+  title = 'm1p10mean-bryan-hiaro';
+  currentPage = 'home';
+  showHome = this.currentPage == 'home';
+  showLogin = this.currentPage == 'login';
+  
+  onDataEvent(data: string) {
+    this.currentPage = data == 'home' ? data : 'login'
+    this.showHome = this.currentPage == 'home'
+    this.showLogin = this.currentPage == 'login'
+>>>>>>> parent of 2262573 (login et inscription)
   }
 
   changePage(pageName: string) {
@@ -32,6 +46,18 @@ export class AppComponent {
   //   this.showLogin = this.currentPage == 'login'
   //   this.showInscription = this.currentPage == 'inscription'
 
+<<<<<<< HEAD
   //         (dataEvent)="onDataEvent($event)" in view
   // }
+=======
+  logout() {
+    this.authService.logout();
+  }
+
+  ngOnInit() {
+    this.apiService.get('').subscribe(data => {
+      console.log(JSON.stringify(data));
+    });
+  }
+>>>>>>> parent of 2262573 (login et inscription)
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+<<<<<<< HEAD
 import { InscriptionComponent } from './components/inscription/inscription.component';
 import { StatistiqueComponent } from './components/statistique/statistique.component';
 import { VoitureComponent } from './components/voiture/voiture.component';
 import { ReparationComponent } from './components/reparation/reparation.component';
 import { FormsModule } from '@angular/forms';
+=======
+import { CreateComponent } from './components/create/create.component';
+import { ReadComponent } from './components/read/read.component';
+import { UpdateComponent } from 'src/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+>>>>>>> parent of 2262573 (login et inscription)
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
+<<<<<<< HEAD
     InscriptionComponent,
     StatistiqueComponent,
     VoitureComponent,
     ReparationComponent,
+=======
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent,
+>>>>>>> parent of 2262573 (login et inscription)
   ],
   imports: [
     BrowserModule,
